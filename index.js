2d0127e7a019db3b18ca1109a13e2c501f52f02e
sanjib <sanjibbarakoti112@gmail.com>
2024-10-19 17:47:44 -0400

    add posts functionalities

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-const express = require('express');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/posts', (req, res) => {
 
+const posts = {};
+
+app.get("/posts", (req, res) => {
+   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post("/posts", async (req, res) => {
+   console.log('received post')
+   const id = randomBytes(4).toString("hex");
+   const { title } = req.body;
 
+   posts[id] = {
+      id,
+      title,
+   };
+
+   try {
+      await axios.post("http://localhost:4005/events", {
+         type: "PostCreated",
+         data: {
+            id,
+            title,
+         },
+      });
+   } catch (error) {
+      console.log("Error in posts services: ", error.message);
+   }
+
+   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+   console.log("Received Event:", req.body.type);
+
+   res.send({});
 });
 
 const port = 4000;
 
 app.listen(port, () => {
-   console.log(`Listening on port ${ port }`);
+   console.log(`Listening on port ${port}`);
 });
